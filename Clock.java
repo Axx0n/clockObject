e3f8babab10a133ccf17b58cd01f5818181665d1
William <willstep13@gmail.com>
2019-04-13 20:40:25 -0500

    Create Clock.java

---
+++ Clock.java
@@ -0,0 +1,30 @@
+public class Clock {
+    private BoundedCounter hours;
+    private BoundedCounter minutes;
+    private BoundedCounter seconds;
+   
+    public Clock(int hoursAtBeginning, int minutesAtBeginning, int secondsAtBeginning) {
+        this.hours = new BoundedCounter(23);
+        this.hours.setValue(hoursAtBeginning);
+        this.minutes = new BoundedCounter(59);
+        this.minutes.setValue(minutesAtBeginning);
+        this.seconds = new BoundedCounter(59);
+        this.seconds.setValue(secondsAtBeginning);
+        // the counters that represent hours, minutes and seconds are created and set to have the correct initial values
+    }
+    
+    public void tick() {
+        seconds.next();
+        if(seconds.getValue() == 0) {
+            minutes.next();
+            if(minutes.getValue() == 0) {
+                hours.next();
+            }
+        }
+    }
+    
+    public String toString() {
+        // returns the string representation
+        return hours + ":" + minutes + ":" + seconds;
+    }
+}
